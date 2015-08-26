de05ba30a657571c037610d95eaf4fd11290da3f
unknown <clebrsonn@gmail.com>
2015-08-26 16:18:36 -0300

    no message

--- bootstrap.php
+++ bootstrap.php
@@ -3,18 +3,17 @@
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\Tools\Setup;
 
+
 require_once dirname(__FILE__) . "/lib/vendor/autoload.php";
+include_once dirname(__FILE__) . "/lib/config/conn.php";
 
 // Create a simple "default" Doctrine ORM configuration for Annotations
 $isDevMode = true;
-$config = Setup::createAnnotationMetadataConfiguration(array(__DIR__."/src/entities"), $isDevMode);
+$entities = Setup::createAnnotationMetadataConfiguration(array(__DIR__ . "/src/entities"), $isDevMode);
 // or if you prefer yaml or XML
 //$config = Setup::createXMLMetadataConfiguration(array(__DIR__."/config/xml"), $isDevMode);
 //$config = Setup::createYAMLMetadataConfiguration(array(__DIR__."/config/yaml"), $isDevMode);
 
-// database configuration parameters
-include_once dirname(__FILE__) . "/lib/config/conn.php";
-
 
 // obtaining the entity manager
-$entityManager = EntityManager::create($conn, $config);
+$entityManager = EntityManager::create(conn::$connection, $entities);
